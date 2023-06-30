2cb9189a3169eba81a6752577f8485070bf2151e
PythonDev <vpython65@gmail.com>
2023-06-30 12:02:01 +0530

    Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from "react"
 
 function App() {
+  const [images, setImages] = useState([])
+  const inputRef = useRef(null)
+
+  const addImage = (image) => {
+    setImages([...images, image])
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(images)
+    inputRef.current.value = ""
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h2>Enter a URL to add an image</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="url" id="url" className="imgurl" onChange={(e) => addImage(e.target.value)} ref={inputRef} />
+          <button type="submit" className="sbutton">Submit</button>
+        </form>
+      </div>
+      <div className="images">
+        {images.map((image, index) => (
+          image ? <img src={image} alt="images" key={index} style={{ height: 200, width: 200 }} /> : ""
+        ))}
+      </div>
+    </>
   );
 }
 
